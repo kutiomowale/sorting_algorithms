0ef0529578463d89c95154a90f6852d895aebe7f
Ransome-Kuti Omowale <omowaleransomekuti@gmail.com>
2023-01-21 17:15:13 -0800

    Add 1-insertion_sort_list.c 1-O

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,71 @@
+#include "sort.h"
+
+/**
+  * swap - A function that swaps two nodes of a doubly linked list
+  *
+  * @b: first node
+  * @c: second node(b->next)
+  */
+void swap(listint_t *b, listint_t *c)
+{
+	if (b && c)
+	{
+		listint_t *a = b->prev;
+		listint_t *d = c->next;
+
+		b->next = d;
+		b->prev = c;
+		c->prev = a;
+		c->next = b;
+		if (a)
+			a->next = c;
+		if (d)
+			d->prev = b;
+	}
+}
+
+/**
+  * insertion_sort_list - A function that sorts a doubly linked
+  * list of integers in ascending order using the Insertion sort algorithm
+  *
+  * @list: A pointer pointing to a pointer pointing
+  * to the first element of the doubly linked list of type listint_t
+  */
+void insertion_sort_list(listint_t **list)
+{
+
+	if (list && *list)
+	{
+		listint_t *right; /* traverses the list towards the right  */
+		listint_t *left; /* traverses the list towards the left */
+		listint_t *temp;
+
+		right = *list;
+		while (right->next)
+		{
+			if (right->n > right->next->n)
+			{
+				left = right->next;
+				if (!(right->prev))
+					*list = left;
+				swap(right, left);
+				print_list(*list);
+				while (left->prev)
+				{
+					if (left->n < left->prev->n)
+					{
+						temp = left->prev;
+						if (!(temp->prev))
+							*list = left;
+						swap(temp, left);
+						print_list(*list);
+					}
+					else
+						break;
+				}
+			}
+			else
+				right = right->next;
+		}
+	}
+}
